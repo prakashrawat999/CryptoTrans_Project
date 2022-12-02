e0c8189ce38ba5d0a83f5de996d459509cc4c503
VineetAjmani001 <vineetajmani25@gmail.com>
2022-12-02 18:08:13 +0530

    Profile page updated

--- ecomm/src/components/profile.jsx
+++ ecomm/src/components/profile.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import Cookies from 'js-cookie';
-import Navigationbar from './Navigationbar';
-import Footermain from './Footermain';
+import "../style/cardprofile.css"
+import Cookies from 'js-cookie'
+import Navigationbar from './Navigationbar'
+import Footermain from './Footermain'
+import { Link } from 'react-router-dom'
 export default function Profile() {
   return (
     <>
     <Navigationbar/>
-    <h1 className='text-5xl font-bold my-16'>Profile</h1>
-        <div className='flex justify-center items-center space-x-20 text-3xl font-bold bg-purple-200 '>
-        <div className='flex flex-col items-start ml-64'>
+    <div className='flex justify-center items-center mt-28  mb-32'>
+    <div className="procard">
+        <h2 className='text-blue-200 mb-10'>👨 My Profile</h2>
+        <div className='flex justify-center items-center'>
+        <div className='flex flex-col items-start mr-10 '>
         <p>Name: </p>
         <p>Email:</p>
-        <p>Contact: </p>
+        <p>Phone: </p>
         </div>
         <div className='flex flex-col items-start'>
         <p>{Cookies.get().Name}</p>
@@ -19,6 +23,8 @@ export default function Profile() {
         <p>{Cookies.get().Phone}</p>
         </div>
         </div>
+    </div>
+    </div>
     <Footermain/>
     </>
   )
